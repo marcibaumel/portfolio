5793bacc3fcea403b61ced2e730bd0ec89be0630
Baumel Márton <marcibaumel@gmail.com>
2022-09-22 21:19:02 +0200

    init

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import './App.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Header } from './components/layout/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
