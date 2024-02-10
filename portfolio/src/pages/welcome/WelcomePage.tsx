be40cc9568c976139875aaacaa8fa89f7b35ad99
Baumel Márton <marcibaumel@gmail.com>
2024-02-10 11:50:31 +0100

    init

--- portfolio/src/pages/welcome/WelcomePage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import classes from './WelcomePage.module.css'
-
-export const WelcomePage = () => {
-  return (
-    <div className={classes.container}>Welcome on my site</div>
-  )
-}
