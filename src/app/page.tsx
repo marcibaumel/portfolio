7d4fc93e59b4d0ba8741c5e1d872d93dd44fe934
Baumel Márton <marcibaumel@gmail.com>
2024-03-04 09:45:03 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import Navbar from '@/components/Navbar/Navbar';
-import { store } from '@/store/store';
-import Image from 'next/image';
-
 export default function Home() {
     return (
         <>
