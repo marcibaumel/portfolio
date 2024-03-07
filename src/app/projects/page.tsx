339839c16e7bdda61b341a34db8925012a0b22f0
Baumel Márton <marcibaumel@gmail.com>
2024-03-07 17:59:47 +0100

    Navbar update

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='h-full'>page</div>
+    <div className='h-screen'>Projects</div>
   )
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,13 +6,16 @@ import { ThemeContext } from '@/contexts/ThemeContext';
 import { TiAdjustBrightness } from 'react-icons/ti';
 import { useTranslation } from '@/contexts/LanguageContext';
 import { WiMoonWaningCrescent2 } from 'react-icons/wi';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import LanguageDropdown from '../LanguageDropdown/LanguageDropdown';
+import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
     const [isHamburgerMenuOpen, setIsHamburgerMenuOpen] = useState(false);
     const [isLanguageDropdownOpen, setIsLanguageDropdownOpen] = useState(false);
     const [isDark, setIsDark] = useState(true);
     const [theme, setTheme] = useState('');
+    const dropdownRef = useRef(null);
     const t = useTranslation();
 
     useEffect(() => {
@@ -21,12 +24,8 @@ const Navbar = () => {
         setTheme(storedTheme);
     }, []);
 
-    const handleOpenHamburgerMenu = () => {
+    const toggleOpenHamburgerMenu = () => {
         setIsHamburgerMenuOpen(!isHamburgerMenuOpen);
-    };
-
-    const handleCloseHamburgerMenu = () => {
-        setIsHamburgerMenuOpen(false);
     };
 
     const handleStyleChange = () => {
@@ -40,45 +39,36 @@ const Navbar = () => {
         setIsDark(!isDark);
     };
 
-    const handleOpenLanguageTab = () => {
-        setIsLanguageDropdownOpen(true);
-    };
-
-    const handleCloseLanguageTab = () => {
-        setIsLanguageDropdownOpen(true);
+    const toggleOpenLanguageTab = () => {
+        setIsLanguageDropdownOpen(false);
     };
 
     const { changeTheme } = useContext(ThemeContext);
     const { changeLanguage } = useContext(ThemeContext);
+    const router = useRouter()
 
     return (
-        <div className='mb-4'>
+        <div
+            className='mb-4'
+            ref={dropdownRef}>
             <div className={`navbar hidden md:flex`}>
-                <div className='flex-1'>
+                <div className='flex-1' onClick={() => router.push('/')}>
                     <a className='btn btn-ghost text-xl'>Baumel Márton</a>
                 </div>
                 <div className='flex-none'>
                     <ul className='menu menu-horizontal px-1'>
+                    <li>
+                            <a onClick={() => router.push('/about')}>{t('about')}</a>
+                        </li>
                         <li>
-                            <a>{t('projects')}</a>
+                            <a onClick={() => router.push('/projects')}>{t('projects')}</a>
                         </li>
 
                         <li>
-                            <details
-                                tabIndex={0}
-                                open={isLanguageDropdownOpen}
-                                onBlur={handleCloseLanguageTab}
-                                onClick={handleOpenLanguageTab}>
-                                <summary>{t('languages')}</summary>
-                                <ul className='p-2 bg-base-100 rounded-t-none'>
-                                    <li>
-                                        <a onClick={() => changeLanguage('en')}>{t('english')}</a>
-                                    </li>
-                                    <li>
-                                        <a onClick={() => changeLanguage('hu')}>{t('hungarian')}</a>
-                                    </li>
-                                </ul>
-                            </details>
+                            <LanguageDropdown
+                                isLarge
+                                changeLanguage={changeLanguage}
+                            />
                         </li>
                         <li>
                             <a onClick={handleStyleChange}>
@@ -102,39 +92,29 @@ const Navbar = () => {
                     <GiHamburgerMenu
                         className='m-4'
                         size={30}
-                        onClick={handleOpenHamburgerMenu}
+                        onClick={toggleOpenHamburgerMenu}
                     />
                 </div>
 
                 {isHamburgerMenuOpen && (
-                    <ul className='menu bg-base-200 rounded-box'>
-                        <li>
-                            <a>{t('projects')}</a>
-                        </li>
+                    <div>
+                        <ul className='menu bg-base-200 rounded-box'>
+                            <li>
+                                <a>{t('projects')}</a>
+                            </li>
 
-                        <li>
-                            <details>
-                                <summary tabIndex={0}>{t('languages')}</summary>
-                                <ul className='p-2 bg-base-100 rounded-t-none'>
-                                    <li>
-                                        <a onClick={() => changeLanguage('en')}>{t('english')}</a>
-                                    </li>
-                                    <li>
-                                        <a onClick={() => changeLanguage('hu')}>{t('hungarian')}</a>
-                                    </li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li>
-                            <a onClick={handleStyleChange}>
-                                {theme === 'light' ? (
-                                    <WiMoonWaningCrescent2 size={20} />
-                                ) : (
-                                    <TiAdjustBrightness size={20} />
-                                )}
-                            </a>
-                        </li>
-                    </ul>
+                            <LanguageDropdown changeLanguage={changeLanguage} />
+                            <li>
+                                <a onClick={handleStyleChange}>
+                                    {theme === 'light' ? (
+                                        <WiMoonWaningCrescent2 size={20} />
+                                    ) : (
+                                        <TiAdjustBrightness size={20} />
+                                    )}
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 )}
             </div>
         </div>
