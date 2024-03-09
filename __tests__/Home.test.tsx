033e88ff7440772d6eb7f5ca1c40cb34a2109694
Marci <marcibaumel@gmail.com>
2024-03-09 21:04:47 +0100

    Tests added

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import Home from '@/app/page'
+import Navbar from '@/components/Navbar/Navbar';
+import ClientThemeWrapper from '@/contexts/ClientThemeWrapper';
+import ThemeProvider from '@/contexts/ThemeContext';
+
+it('Should have CV text ', () => {
+    render(<ThemeProvider>
+        <ClientThemeWrapper>
+            <Home />
+        </ClientThemeWrapper>
+    </ThemeProvider>)
+    const myElem = screen.getByText('CV content');
+    expect(myElem).toBeInTheDocument()
+})
